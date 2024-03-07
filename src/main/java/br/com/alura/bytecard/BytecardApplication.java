fb06e6529c40de076cfb087b779d74efcfeee514
Leonardo Silva <leonardo.inacio@ifood.com.br>
2024-03-07 15:23:06 -0300

    Versão inicial do projeto

---
+++ src/main/java/br/com/alura/bytecard/BytecardApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.bytecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytecardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytecardApplication.class, args);
+	}
+
+}
